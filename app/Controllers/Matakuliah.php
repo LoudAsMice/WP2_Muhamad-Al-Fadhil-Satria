54ea1c4f65db95a54935e0a730921267dc876cb8
LoudAsMice <fadhilsatria789@gmail.com>
2022-04-12 17:14:46 +0800

    validasi (pertemuan 5)

--- app/Controllers/Matakuliah.php
+++ app/Controllers/Matakuliah.php
@@ -2,8 +2,11 @@
 
 namespace App\Controllers;
 
+use CodeIgniter\HTTP\URI;
+
 class Matakuliah extends BaseController
 {
+    
     public function index()
     {
         return view('view_form_matakuliah');
@@ -11,6 +14,41 @@ class Matakuliah extends BaseController
 
     public function cetak()
     {
+        $validasi = \Config\Services::validation();
+
+        // $validasi->setRule('kode', 'Kode Matakuliah', 'required|min_length[2]',[
+        //     'required' => 'Kode Matakuliah harus diisi',
+        //     'min_length' => 'Kode terlalu pendek'
+        // ]);
+
+        // $validasi->setRule('nama', 'Nama Matakuliah', 'required|min_length[2]',[
+        //     'required' => 'Nama Matakuliah harus diisi',
+        //     'min_length' => 'Nama terlalu pendek'
+        // ]);
+
+        // if($validasi->run() != true){
+        //     // return view('view_form_matakuliah');
+        //     echo $validasi->listErrors();
+
+        $rules = [
+            "kode" => "required|min_length[3]",
+            "nama" => "required|min_length[3]"
+        ];
+        $message = [
+            "kode" => [
+                "required" => "Kode Matakuliah Harus Diisi",
+                "min_length" => "Kode terlalu pendek"
+            ],
+            "nama" => [
+                "required" => "Nama Harus Diisi",
+                "min_length" => "Nama terlalu pendek"
+            ]
+        ];
+        if(!$this->validate($rules,$message)){
+            return redirect()->to('http://localhost/WP2_Muhamad-Al-Fadhil-Satria/public/matakuliah');
+            // echo $validasi->listErrors();
+        }else{
+
         $data = [
             'kode' => $_POST["kode"],
             'nama' => $_POST["nama"],
@@ -18,5 +56,7 @@ class Matakuliah extends BaseController
         ];
 
         return view('view_data_matakuliah', $data);
+
+        }
     }
 }
--- app/Views/view_form_matakuliah.php
+++ app/Views/view_form_matakuliah.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <center>
-    <form action="matakuliah/cetak" method="POST">
+    <form action="http://localhost/WP2_Muhamad-Al-Fadhil-Satria/public/matakuliah/cetak" method="POST">
         <table>
             <tr>
                 <th colspan="3">
@@ -24,21 +24,21 @@
                 <th>Kode MTK</th>
                 <th>:</th>
                 <td>
-                    <input type="text" name="kode" id="kode" required>
+                    <input type="text" name="kode" id="kode" >
                 </td>
             </tr>
             <tr>
                 <th>Nama MTK</th>
                 <th>:</th>
                 <td>
-                    <input type="text" name="nama" id="nama" required>
+                    <input type="text" name="nama" id="nama" >
                 </td>
             </tr>
             <tr>
                 <th>SKS</th>
                 <th>:</th>
                 <td>
-                    <select name="sks" id="sks" required>
+                    <select name="sks" id="sks" >
                         <option value="">Pilih SKS</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
